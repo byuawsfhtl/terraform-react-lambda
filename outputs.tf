efd89a52773f3b00963f2a712f4894a74bbfd31a
jnienhui <jnienhuis132@gmail.com>
2025-01-13 12:33:32 -0700

    add output

--- outputs.tf
+++ outputs.tf
@@ -9,3 +9,8 @@ output "cf_distribution_id" {
 output "url" {
   value = local.url
 }
+
+output "lambda_api_lambda_arns" {
+    description = "The ARNs of the Lambda functions from all api_endpoint instances"
+    value       = module.lambda_api.lambda_api_lambda_arns
+}
